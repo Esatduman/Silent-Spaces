52396c0112ca539f915a0602748e402033e4260f
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-02-11 02:25:21 -0600

    Basic email+pass signup page.

--- Coding Project/silent-spaces/src/routes/Signup.jsx
+++ Coding Project/silent-spaces/src/routes/Signup.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 export function Signup() {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const auth = getAuth();
+    
+    async function handleSignUp() {
+        createUserWithEmailAndPassword(auth, email, pass)
+        .then((user) => {
+            console.log(user)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    };
+
     return (
         <div>
             <h1>Sign Up.</h1>
             <form>
-                <input type="text" placeholder="email address"></input>
-                <input type="text" placeholder="password"></input>
-                <button>Sign Up</button>
+                <input onChange={(e) => {setEmail(e.target.value)}} type="text" placeholder="email address"></input>
+                <input onChange={(e) => {setPass(e.target.value)}} type="text" placeholder="password"></input>
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    handleSignUp()
+                    }}>Sign Up</button>
             </form>
         </div>
     );
