3ac5bcbfa6ed68c4eb18d7f29665e81cc5c67616
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-02-11 02:35:07 -0600

    Basic logout button.

--- Coding Project/silent-spaces/src/routes/Home.jsx
+++ Coding Project/silent-spaces/src/routes/Home.jsx
@@ -1,3 +1,18 @@
+import { signOut, getAuth } from "firebase/auth"
+
 export function Home() {
-    return <h1>Home.</h1>;
+    const auth = getAuth()
+
+    async function handleLogout() {
+        try {
+            await signOut(auth)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return <div>
+        <h1>Home.</h1>
+        <button onClick={() => {handleLogout()}}>Logout.</button>
+    </div>
 }
