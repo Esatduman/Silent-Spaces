e3a4237beed96bca9b88f935c1d25a33bbf62427
Esat Duman <esatduman214@gmail.com>
2024-02-29 20:21:35 -0600

    made some changes to the App.css

--- Coding Project/silent-spaces/src/main.jsx
+++ Coding Project/silent-spaces/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import 'bootstrap/dist/css/bootstrap.css'; // CSS here added btw - Esat 
 import firebase_cfg from "./firebase_cfg.js";
 import { initializeApp } from "firebase/app";
 
