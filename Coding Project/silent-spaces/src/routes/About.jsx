53834a4715ad94bee30ae518e8763eb77b4f98ab
jjuarez19 <89400644+jjuarez19@users.noreply.github.com>
2024-03-28 23:37:37 -0500

    updated about page

--- Coding Project/silent-spaces/src/routes/About.jsx
+++ Coding Project/silent-spaces/src/routes/About.jsx
@@ -1,7 +1,21 @@
+import "./About.scss";
+
 export function About() {
     return <>
-    <h1>About</h1>
 
-    
+    <div class = "title-div">
+        <h1 class = "first-header">Meet the Team</h1>
+    </div>
+
+    <div class = "names-div">
+        <h2 class = "name-header">Esat Duman - eduman2@uic.edu</h2>
+
+        <h2 class = "name-header">Jonathan Juarez - jjuar2@uic.edu</h2>
+
+        <h2 class = "name-header">Kent Lizardo - kliza2@uic.edu</h2>
+
+        <h2 class = "name-header">Jose Tejeda - jtejed4@uic.edu</h2>
+    </div>
+
     </>
 }
