8f560758f754034990013c77f0ceecd769fe481a
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-03-29 02:31:11 -0500

    Implemented Spacelist.jsx

--- Coding Project/silent-spaces/src/components/SpaceCard.jsx
+++ Coding Project/silent-spaces/src/components/SpaceCard.jsx
@@ -1,7 +1,23 @@
+import { getFirestore, getDoc, doc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
 export function SpaceCard({space}) {
+    const db = getFirestore();
+    const [username, setUsername] = useState("");
+
+    const retrieveUsername = async() => {
+        const usernameRef = doc(db, "users", space.owner);
+        const usernameSnap = await getDoc(usernameRef);
+        setUsername(usernameSnap.data().username);
+    }
+
+    useEffect(() => {
+        retrieveUsername();
+    }, []);
     return (<>
+    {JSON.stringify(space)}
     <div className="space-card">
-        @{space.owner}:{space.name}
+        @{username}:{space.name}
         <br />
         {space.displayName}
     </div>
--- Coding Project/silent-spaces/src/components/SpaceList.jsx
+++ Coding Project/silent-spaces/src/components/SpaceList.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SpaceCard } from "./SpaceCard";
 import {APIProvider, Map, Marker, AdvancedMarker, InfoWindow, useAdvancedMarkerRef} from '@vis.gl/react-google-maps';
 import useGeoLocation from "./useGeoLocation";
+import { collection, query, orderBy, startAt, endAt, getDocs, getFirestore } from 'firebase/firestore';
+import { geohashQueryBounds, distanceBetween } from "geofire-common";
 
 const dummySpaceViewData = {
     spaces: [
@@ -29,18 +31,63 @@ const dummySpaceViewData = {
 const defaultLoc = { lat: 41.86867120799108, lng: -87.64836798595782 };
 
 export function SpaceList({spaceViewData}) {
+    const db = getFirestore();
     const { getLocation, location, error } = useGeoLocation();
     const viewData = dummySpaceViewData;
+    
+    const retrieveLocations = async (center) => {
+        const radiusInM = 50 * 1000;
+        const bounds = geohashQueryBounds(center, radiusInM);
+        const promises = [];
+        for (const b of bounds) {
+            const q = query(
+                collection(db, 'spaces'), 
+                orderBy('geohash'), 
+                startAt(b[0]), 
+                endAt(b[1]));
+            promises.push(getDocs(q));
+        }
+        const snapshots = await Promise.all(promises);
+        const matchingDocs = [];
+        for (const snap of snapshots) {
+            for (const doc of snap.docs) {
+                const lat = doc.get('lat');
+                const lng = doc.get('lng');
+                const distanceInKm = distanceBetween([lat, lng], center);
+                const distanceInM = distanceInKm * 1000;
+                if (distanceInM <= radiusInM) {
+                matchingDocs.push(doc);
+                }
+            }
+        }
+        const allSpaceDatas = [];
+        for(const doc of matchingDocs) {
+            allSpaceDatas.push(doc.data());
+        }
+        setSpaces(allSpaceDatas);
+        return matchingDocs;
+    }
+    
+    useEffect(() => {
+        getLocation();
+    }, []);
+    useEffect(() => {
+        const getData = setTimeout(async() => {
+            retrieveLocations([location.latitude, location.longitude]);
+        }, 500)
+        return () => clearTimeout(getData)
+    }, [location]);
 
+    const [spaces, setSpaces] = useState([]);
     const [showSpaceInfo, setShowSpaceInfo] = useState(true);
     const [markerRef, marker] = useAdvancedMarkerRef();
     
+
     return (<>
     <div className="space-list">
         <div className="space-list-left">
-            
         <ul>
-        {viewData.spaces.map((space) => <li key={space.name}><SpaceCard space={space}></SpaceCard></li>)}
+        {spaces.map((space) => <li key={space.name}><SpaceCard space={space}></SpaceCard></li>)}
         </ul>
         </div>
         <div className="space-list-right">
