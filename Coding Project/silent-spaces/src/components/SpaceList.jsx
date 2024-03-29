6f372daf3d14081465f2f36fccd697dd20b5729b
jjuarez19 <89400644+jjuarez19@users.noreply.github.com>
2024-03-29 02:04:38 -0500

    idk

--- Coding Project/silent-spaces/src/components/SpaceList.jsx
+++ Coding Project/silent-spaces/src/components/SpaceList.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { SpaceCard } from "./SpaceCard";
-import {APIProvider, Map, Marker, AdvancedMarker, InfoWindow, useAdvancedMarkerRef} from '@vis.gl/react-google-maps';
-import useGeoLocation from "./useGeoLocation";
+import GoogleMapAPI from "./GoogleMapAPI";
 
 const dummySpaceViewData = {
     spaces: [
@@ -26,50 +24,17 @@ const dummySpaceViewData = {
     ]
 }
 
-const defaultLoc = { lat: 41.86867120799108, lng: -87.64836798595782 };
-
 export function SpaceList({spaceViewData}) {
-    const { getLocation, location, error } = useGeoLocation();
     const viewData = dummySpaceViewData;
-
-    const [showSpaceInfo, setShowSpaceInfo] = useState(true);
-    const [markerRef, marker] = useAdvancedMarkerRef();
-    
     return (<>
     <div className="space-list">
         <div className="space-list-left">
-            
         <ul>
-        {viewData.spaces.map((space) => <li key={space.name}><SpaceCard space={space}></SpaceCard></li>)}
+        {viewData.spaces.map((space) => <li><SpaceCard space={space}></SpaceCard></li>)}
         </ul>
         </div>
         <div className="space-list-right">
-        <APIProvider apiKey={'AIzaSyC26_AOm2ZW6U8CbYkjtwwk2WEN09FAAUg'}>
-            <Map defaultCenter={defaultLoc} defaultZoom={80}>
-            <Marker position={defaultLoc} />
-
-            {viewData.spaces.map((space) =>
-            <>
-            <AdvancedMarker
-            ref={markerRef}
-            onClick={() => setShowSpaceInfo(true)}
-            position={{lat: 28, lng: -82}}
-            title={'AdvancedMarker that opens an Infowindow when clicked.'} />
-            {showSpaceInfo && (
-            <InfoWindow
-            anchor={marker}
-            maxWidth={200}
-            onCloseClick={() => setShowSpaceInfo(false)}>
-            This is an example for the{' '}
-            <code style={{whiteSpace: 'nowrap'}}>&lt;AdvancedMarker /&gt;</code>{' '}
-            combined with an Infowindow.
-            </InfoWindow>
-            )}
-            </>
-            )}
-            
-            </Map>
-        </APIProvider>
+        <GoogleMapAPI /> 
         </div>
     </div>
     </>);
--- Coding Project/silent-spaces/src/routes/Dashboard.jsx
+++ Coding Project/silent-spaces/src/routes/Dashboard.jsx
@@ -16,15 +16,8 @@ export function Dashboard() {
         <div>
             {outlet || <>Error: Unknown dashboard page.</>}
         </div>
+
     </div>
-    
-    {/* <div className="card">
-      <img src="img_avatar.png" alt="Avatar" style={{width: '100%'}} />
-      <div className="container">
-        <h4><b>John Doe</b></h4> 
-        <p>Architect & Engineer</p> 
-      </div>
-    </div> */}
         
     </>
     );
