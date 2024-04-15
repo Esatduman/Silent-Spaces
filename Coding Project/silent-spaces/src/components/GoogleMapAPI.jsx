751a3ee9329d155f2d424418287f228ce4aef6c0
Esat Duman <esatduman214@gmail.com>
2024-04-15 14:33:50 -0500

    googleAPIStyling

--- Coding Project/silent-spaces/src/components/GoogleMapAPI.jsx
+++ Coding Project/silent-spaces/src/components/GoogleMapAPI.jsx
@@ -6,15 +6,26 @@ const center = {
   lng: -87.64836798595782, // default longitude
 };
 
+const mapStyles = [
+  {
+    featureType: 'poi',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }],
+  },
+];
+
+
 
 export default function GoogleMapAPI () {
   return (
         <APIProvider apiKey={'AIzaSyC26_AOm2ZW6U8CbYkjtwwk2WEN09FAAUg'}>
-        <Map defaultCenter={center} defaultZoom={16} style={{ width: '100%', height: '100%' }} >
+        <Map defaultCenter={center} defaultZoom={10} style={{ width: '100%', height: '100%' }} 
+        options={{styles: mapStyles}}>
         <Marker position={center} />
         </Map>
         </APIProvider>
   );
+ 
 
 };
 
