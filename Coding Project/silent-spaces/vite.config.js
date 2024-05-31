12bf750309ea51ad864221ceeed00a3e1f00ede1
Esat Duman <esatduman214@gmail.com>
2024-05-30 20:52:05 -0500

    deploy

--- Coding Project/silent-spaces/vite.config.js
+++ Coding Project/silent-spaces/vite.config.js
@@ -4,6 +4,7 @@ import {fileURLToPath} from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Silent-Spaces/',  // Add the base URL here
   plugins: [react()],
   resolve: {
     alias: [
