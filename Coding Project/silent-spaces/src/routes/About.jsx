560f7f5b106763af22c3e3ace2efcfd19b78f10e
Esat Duman <102714064+Esatduman@users.noreply.github.com>
2025-02-03 13:42:07 -0600

    more updates

--- Coding Project/silent-spaces/src/routes/About.jsx
+++ Coding Project/silent-spaces/src/routes/About.jsx
@@ -5,10 +5,9 @@ export function About() {
     return (
         <>
             <div className="title-div">
-               <p>
-                Silent Spaces is all about helping people find silent places to study,gather with friends,rest and locate peace and quiet in crowded areas that are typically make it hard
-                to find a nice and quiet spot. 
-               </p>
+               <h2>
+               Silent Spaces is a platform designed to help users find quiet locations for studying, gathering with friends, resting, and seeking peace in typically crowded areas. Built with React.js and Firebase, the application integrates the Google Maps API to provide an intuitive and efficient way to discover and navigate to silent spaces.
+               </h2>
             </div>
         </>
     );
