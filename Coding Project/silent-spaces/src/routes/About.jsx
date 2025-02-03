1c1fa095ec65afdb6d05b3bb18170b410b0a4c22
Esat Duman <102714064+Esatduman@users.noreply.github.com>
2025-02-03 13:35:26 -0600

    updates

--- Coding Project/silent-spaces/src/routes/About.jsx
+++ Coding Project/silent-spaces/src/routes/About.jsx
@@ -1,38 +1,14 @@
 import "./About.scss";
 
 export function About() {
-    const PEOPLE = [
-        {
-            name: "Esat Duman",
-            email: "eduman2@uic.edu",
-        },
-        {
-            name: "Jonathan Juarez",
-            email: "jjuar2@uic.edu",
-        },
-        {
-            name: "Kent Lizardo",
-            email: "kliza2@uic.edu",
-        },
-        {
-            name: "Jose Tejeda",
-            email: "jtejed4@uic.edu"
-        }
-    ];
-
+    
     return (
         <>
             <div className="title-div">
-                <h1 className="header">Meet the Team</h1>
-            </div>
-
-            <div className="names-div">
-                {PEOPLE.map((person, index) => (
-                    <div className="person-div" key={index}>
-                        <h3>{person.name}</h3>
-                        <p>{person.email}</p>
-                    </div>
-                ))}
+               <p>
+                Silent Spaces is all about helping people find silent places to study,gather with friends,rest and locate peace and quiet in crowded areas that are typically make it hard
+                to find a nice and quiet spot. 
+               </p>
             </div>
         </>
     );
